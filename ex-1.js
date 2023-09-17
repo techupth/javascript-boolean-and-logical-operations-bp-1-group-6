d66a97f20834cea7ca2af2078def26eded8e8f38
Marisa Phungbunphra <marisamie30@gmail.com>
2023-09-17 17:47:42 +0700

    feat: assignment from mie

--- ex-1.js
+++ ex-1.js
@@ -1,10 +1,10 @@
 // Exercise #1: Event Conditions
 
 // Start coding here
-let isOver18 = 19;
-let hasCriminalBlacklist;
-let isThai;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
+let isThai = true;
 
-let isAllow;
+let isAllow = isOver18 && !isThai && !hasCriminalBlacklist;
 
 console.log(isAllow);
