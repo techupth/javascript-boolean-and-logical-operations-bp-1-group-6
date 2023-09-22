ccf262b9eb8d12a0e9736085368c6a49a00e9be7
Marisa Phungbunphra <marisamie30@gmail.com>
2023-09-22 18:59:37 +0700

    feat: assignment Boolean from mie

--- ex-1.js
+++ ex-1.js
@@ -5,6 +5,6 @@ let isOver18 = true;
 let hasCriminalBlacklist = true;
 let isThai = true;
 
-let isAllow = isOver18 && !isThai && !hasCriminalBlacklist;
+let isAllow = isOver18 || !isThai || !hasCriminalBlacklist;
 
 console.log(isAllow);
